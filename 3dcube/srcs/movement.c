9b193f049a99d25e698af0e58d158e1593406465
Jason Williamson <jwilliam@F3C022.42adel.org.au>
2023-02-03 14:26:51 +1030

    collision works in theory

--- 3dcube/srcs/movement.c
+++ 3dcube/srcs/movement.c
@@ -6,7 +6,7 @@
 /*   By: jwilliam <jwilliam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 11:37:53 by tbertozz          #+#    #+#             */
-/*   Updated: 2023/02/03 13:43:41 by jwilliam         ###   ########.fr       */
+/*   Updated: 2023/02/03 14:25:01 by jwilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,26 +31,102 @@ int		move_loop(t_game *game)
 
 void	move_forward(t_game *game)
 {
+	double	new_x;
+	double	new_y;
+
+	new_x = game->doom_guy.x;
+	new_y = game->doom_guy.y;
 	printf("Moving forward toward %f at a speed of %f\n",
 		game->doom_guy.direction.x, game->doom_guy.speed);
-	game->doom_guy.x += (game->doom_guy.direction.x * game->doom_guy.speed);
-	game->doom_guy.y += (game->doom_guy.direction.y * game->doom_guy.speed);
+	new_x += (game->doom_guy.direction.x * game->doom_guy.speed);
+	new_y += (game->doom_guy.direction.y * game->doom_guy.speed);
+	if ((int)new_x != (int)game->doom_guy.x)
+	{
+		if (game->tilemap[(int)game->doom_guy.y][(int)new_x].type != WALL)
+		game->doom_guy.x = new_x;
+	}
+	else
+		game->doom_guy.x = new_x;
+	if ((int)new_y != (int)game->doom_guy.y)
+	{
+		if (game->tilemap[(int)new_y][(int)game->doom_guy.x].type != WALL)
+		game->doom_guy.y = new_y;
+	}
+	else
+		game->doom_guy.y = new_y;
 }
 
 void	move_backward(t_game *game)
 {
-	game->doom_guy.x -= game->doom_guy.direction.x * game->doom_guy.speed;
-	game->doom_guy.y -= game->doom_guy.direction.y * game->doom_guy.speed;
+	double	new_x;
+	double	new_y;
+
+	new_x = game->doom_guy.x;
+	new_y = game->doom_guy.y;
+	new_x -= game->doom_guy.direction.x * game->doom_guy.speed;
+	new_y -= game->doom_guy.direction.y * game->doom_guy.speed;
+	if ((int)new_x != (int)game->doom_guy.x)
+	{
+		if (game->tilemap[(int)game->doom_guy.y][(int)new_x].type != WALL)
+		game->doom_guy.x = new_x;
+	}
+	else
+		game->doom_guy.x = new_x;
+	if ((int)new_y != (int)game->doom_guy.y)
+	{
+		if (game->tilemap[(int)new_y][(int)game->doom_guy.x].type != WALL)
+		game->doom_guy.y = new_y;
+	}
+	else
+		game->doom_guy.y = new_y;
 }
 
 void	strafe_left(t_game *game)
 {
-	game->doom_guy.x += game->doom_guy.strafe.x * game->doom_guy.speed;
-	game->doom_guy.y += game->doom_guy.strafe.y * game->doom_guy.speed;
+	double	new_x;
+	double	new_y;
+
+	new_x = game->doom_guy.x;
+	new_y = game->doom_guy.y;
+	new_x += game->doom_guy.strafe.x * game->doom_guy.speed;
+	new_y += game->doom_guy.strafe.y * game->doom_guy.speed;
+	if ((int)new_x != (int)game->doom_guy.x)
+	{
+		if (game->tilemap[(int)game->doom_guy.y][(int)new_x].type != WALL)
+		game->doom_guy.x = new_x;
+	}
+	else
+		game->doom_guy.x = new_x;
+	if ((int)new_y != (int)game->doom_guy.y)
+	{
+		if (game->tilemap[(int)new_y][(int)game->doom_guy.x].type != WALL)
+		game->doom_guy.y = new_y;
+	}
+	else
+		game->doom_guy.y = new_y;
 }
 
 void	strafe_right(t_game *game)
 {
-	game->doom_guy.x -= (game->doom_guy.strafe.x * game->doom_guy.speed);
-	game->doom_guy.y -= (game->doom_guy.strafe.y * game->doom_guy.speed);
+	double	new_x;
+	double	new_y;
+
+	new_x = game->doom_guy.x;
+	new_y = game->doom_guy.y;
+	new_x -= (game->doom_guy.strafe.x * game->doom_guy.speed);
+	new_y -= (game->doom_guy.strafe.y * game->doom_guy.speed);
+	if ((int)new_x != (int)game->doom_guy.x)
+	{
+		if (game->tilemap[(int)game->doom_guy.y][(int)new_x].type != WALL)
+		game->doom_guy.x = new_x;
+	}
+	else
+		game->doom_guy.x = new_x;
+	if ((int)new_y != (int)game->doom_guy.y)
+	{
+		if (game->tilemap[(int)new_y][(int)game->doom_guy.x].type != WALL)
+		game->doom_guy.y = new_y;
+	}
+	else
+		game->doom_guy.y = new_y;
 }
